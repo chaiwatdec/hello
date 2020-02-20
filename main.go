2207cd73c5fe90f0ff88c7246159a3b8ba30b080
chaiwatdec <chaiwat.dec@gmail.com>
2020-02-20 21:36:28 +0700

    commit

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello")
-
+	var num int = 100
+	num2 := 300
+	something := true
+	text := "tum"
+	age1,age2:=100,200
+	fmt.Println("Num = ",num)
+	fmt.Println("Num2 = ",num2)
+	fmt.Println("Boo = ",something)
+	fmt.Println(text)
+	fmt.Println(age1,age2)
 }
