1a706185d6a0c5631ff7a3d97a2d1d84e0ae136f
chaiwatdec <chaiwat.dec@gmail.com>
2020-02-25 21:41:42 +0700

    commit

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	var num int = 100
-	num2 := 300
-	something := true
-	text := "tum"
-	age1,age2:=100,200
-	fmt.Println("Num = ",num)
-	fmt.Println("Num2 = ",num2)
-	fmt.Println("Boo = ",something)
-	fmt.Println(text)
-	fmt.Println(age1,age2)
+func main(){
+	fmt.Println(factorial(5))
+}
+
+func factorial(num int) int{
+	if num==0 {
+		return 1
+	}
+	return num*factorial(num-1)
 }
