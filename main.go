d5d8d4339065f9f35ba7b743a403d9866127b647
chaiwatdec <chaiwat.dec@gmail.com>
2020-02-27 21:10:49 +0700

    commit

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	slice1 := []int{1, 2, 3}
-	slice2 := make([]int, 2) //slice fix 2
-	copy(slice2, slice1)     //copy fr slice1 to slice2
-	fmt.Println(slice2)
+	x := []int{1, 2, 3}
+	fmt.Println(x)
 }
