64b1d5efe850efd0ceba8e315d45c65e568bc1c9
chaiwatdec <chaiwat.dec@gmail.com>
2020-02-28 21:38:39 +0700

    commit

--- main.go
+++ main.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	x := []int{1, 2, 3}
-	fmt.Println(x)
+	x:=10
+	fmt.Printf("Value of x is %d\n",x)
+	fmt.Printf("Address of x = %x\n",&x)
+
+	var p *int
+	p=&x	//pointer p get address var x
+	fmt.Printf("Value of p = %x\n",p)
+	fmt.Printf("Address of p = %x\n",&p)
+
+
+	*p=20	//pointer p change value of x
+	fmt.Printf("Value of x = %d\n",x) //after change value by pointer
 }
+
