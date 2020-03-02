f8599de4cc2d8960164f8b145f8c56f91c842131
chaiwatdec <chaiwat.dec@gmail.com>
2020-03-02 21:47:38 +0700

    commit

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
-
+import s "strings"
 import "fmt"
 
+var p = fmt.Println
 func main() {
-	x:=10
-	fmt.Printf("Value of x is %d\n",x)
-	fmt.Printf("Address of x = %x\n",&x)
-
-	var p *int
-	p=&x	//pointer p get address var x
-	fmt.Printf("Value of p = %x\n",p)
-	fmt.Printf("Address of p = %x\n",&p)
-
-
-	*p=20	//pointer p change value of x
-	fmt.Printf("Value of x = %d\n",x) //after change value by pointer
+	p("Contains:	", s.Contains("test", "es"))
+	p("Count:		", s.Count("test", "t"))	//count t
+	p("HasPrefix:	", s.HasPrefix("test", "te"))	//start with te?
+	p("HasSuffix:	", s.HasSuffix("test", "st"))	//end with st?
+	p("Index:		", s.Index("test", "t"))	//find position
+	p("Join:		", s.Join([]string{"a", "b"}, "-")) //join a a with -
+	p("Repeat:		", s.Repeat("a", 5)) //repeat 1 5 times
+	p("Replace:	", s.Replace("foo", "o", "0", -1)) 
+	p("Replace:	", s.Replace("fooo", "o", "0", 2))	//replace o with 0 2 time
+	p("Split:		", s.Split("a-b-c-d-e", "-")) //split with -
+	p("ToUpper:	", s.ToUpper("test"))
+	p("Len:		", len("hello")) //count string
 }
 
